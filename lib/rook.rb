f9503871717dc4ea1e7a03726087abb1670b976f
Brittany Woedl <bwoedl4@gmail.com>
2022-05-29 10:04:17 -0500

    Add method to check if move is legal for rook

--- lib/rook.rb
+++ lib/rook.rb
@@ -9,4 +9,8 @@ class Rook < Piece
   def set_symbol
     color == 'white' ? '♜'.bold : '♜'.black
   end
+
+  def self.legal_move?(start_spot, end_spot)
+    start_spot[0] == end_spot[0] || start_spot[1] == end_spot[1]
+  end
 end
---
+++ spec/rook_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'rook'
+
+describe Rook do
+  describe '.legal_move' do
+    context 'moves diagonally' do
+      it 'returns false' do
+        expect(Rook.legal_move?([4, 3], [5, 4])).to be(false)
+      end
+    end
+    context 'moves one space to the side' do
+      it 'returns true' do
+        expect(Rook.legal_move?([4, 3], [4, 4])).to be(true)
+      end
+    end
+    context 'moves multiple spaces to the side' do
+      it 'returns true' do
+        expect(Rook.legal_move?([1, 3], [7, 3])).to be(true)
+      end
+    end
+  end
+end
