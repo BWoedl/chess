66a1514419da5f0d5d716d124470e00a30db8be8
Brittany Woedl <bwoedl4@gmail.com>
2022-05-30 17:54:23 -0500

    Update legal move method to check if target spot is occupied by piece of same color

--- lib/pawn.rb
+++ lib/pawn.rb
@@ -1,12 +1,32 @@
 class Pawn < Piece
-  attr_accessor :symbol
+  attr_accessor :symbol, :move
 
   def initialize(color)
     super(color)
     @symbol = set_symbol
+    @move = 1
   end
 
   def set_symbol
     color == 'white' ? '♟'.bold : '♟'.black
+  end
+
+  def legal_move?(board, start_spot, end_spot)
+    row_move = end_spot[0] - start_spot[0]
+    return true if diagonal_move?(start_spot, end_spot) && occupied_by_same_color?(board, end_spot) #and row_move.abs = 1
+    return false if occupied_by_same_color?(board, end_spot)
+    return legal_first_move?(board, start_spot, end_spot) if move == 1
+    return true if color == 'black' && start_spot[1] == end_spot[1] && row_move == -1
+    return true if color == 'white' && start_spot[1] == end_spot[1] && row_move == 1
+
+    false
+  end
+
+  def legal_first_move?(board, start_spot, end_spot)
+    row_move = end_spot[0] - start_spot[0]
+    return true if color == 'black' && start_spot[1] == end_spot[1] && row_move.between?(-2, -1)
+    return true if color == 'white' && start_spot[1] == end_spot[1] && row_move.between?(1, 2)
+
+    false
   end
 end
--- spec/pawn_spec.rb
+++ spec/pawn_spec.rb
@@ -4,57 +4,69 @@ require 'piece'
 require 'pawn'
 
 describe Pawn do
-  xdescribe '.legal_move? for white pawn' do
-    context 'moves one space forward' do
-      it 'returns false' do
-        expect(Pawn.legal_move?([3, 3], [4, 3])).to be(true)
+  let(:white_pawn) { Pawn.new('white') }
+  let(:black_pawn) { Pawn.new('black') }
+  let(:board) { double('board') }
+
+  describe '.legal_move? for black pawn' do
+    context 'moves one space down the board' do
+      it 'returns true' do
+        allow(board).to receive(:get_piece).and_return(nil)
+        expect(black_pawn.legal_move?(board, [4, 3], [3, 3])).to be(true)
       end
     end
     context 'moves two spaces on the first move' do
       it 'returns true' do
-        expect(Pawn.legal_move?([4, 3], [5, 3])).to be(true)
+        black_pawn.instance_variable_set(:@move, 1)
+        allow(board).to receive(:get_piece).and_return(nil)
+        expect(black_pawn.legal_move?(board, [6, 3], [4, 3])).to be(true)
       end
     end
     context 'moves two spaces on a move besides the first' do
       it 'returns false' do
-        #move count > 1
-        expect(Pawn.legal_move?([4, 3], [5, 3])).to be(false)
+        black_pawn.instance_variable_set(:@move, 2)
+        allow(board).to receive(:get_piece).and_return(nil)
+        expect(black_pawn.legal_move?(board, [6, 3], [4, 3])).to be(false)
       end
     end
     context 'moves diagonally when no piece in the target spot' do
       it 'returns false' do
         #target spot empty
-        expect(Pawn.legal_move?([4, 3], [5, 4])).to be(false)
+        allow(board).to receive(:get_piece).and_return(nil)
+        expect(black_pawn.legal_move?(board, [5, 4], [4, 3])).to be(false)
       end
     end
-    context 'moves diagonally when a piece in the target spot' do
+    xcontext 'moves diagonally when a piece in the target spot' do
       it 'returns true' do
         # target spot occupied
-        expect(Pawn.legal_move?([4, 3], [5, 4])).to be(true)
+        allow(board).to receive(:get_piece).and_return(white_pawn)
+        expect(black_pawn.legal_move?(board, [5, 4], [4, 3])).to be(true)
       end
     end
     context 'moves diagonally more than one row' do
       it 'returns false' do
-        # target spot occupied
-        expect(Pawn.legal_move?([4, 3], [6, 5])).to be(false)
-      end
-    end
-    context 'moves forward more than two spaces' do
-      it 'returns false' do
-        # first move
-        expect(Pawn.legal_move?([4, 3], [7, 3])).to be(false)
+        allow(board).to receive(:get_piece).and_return(white_pawn)
+        expect(black_pawn.legal_move?(board, [6, 5], [4, 3])).to be(false)
       end
     end
     context 'moves backward' do
       it 'returns false' do
-        expect(Pawn.legal_move?([4, 3], [3, 3])).to be(false)
+        allow(board).to receive(:get_piece).and_return(nil)
+        expect(black_pawn.legal_move?(board, [2, 3], [3, 3])).to be(false)
       end
     end
   end
-  xdescribe '.legal_move? for black pawn' do
-    context 'moves backward' do
+  describe '.legal_move? for white pawn' do
+    context 'moves down the board' do
       it 'returns false' do
-        expect(Pawn.legal_move?([4, 3], [5, 3])).to be(false)
+        allow(board).to receive(:get_piece).and_return(nil)
+        expect(white_pawn.legal_move?(board, [2, 4], [1, 4])).to be(false)
+      end
+    end
+    context 'moves up the board' do
+      it 'returns true' do
+        allow(board).to receive(:get_piece).and_return(nil)
+        expect(white_pawn.legal_move?(board, [2, 3], [3, 3])).to be(true)
       end
     end
   end
