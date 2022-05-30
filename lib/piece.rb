d8ad2efe733d3853de3e61c7c287162c46c5c1fb
Brittany Woedl <bwoedl4@gmail.com>
2022-05-30 11:24:51 -0500

    Add methods to check if target spot is occupied by same color piece and if a move is diagonal

--- lib/piece.rb
+++ lib/piece.rb
@@ -5,4 +5,15 @@ class Piece
     @color = color
     @defeated = defeated
   end
+
+  def occupied_by_same_color?(board, end_spot)
+    target_spot_piece = board.get_piece(end_spot)
+    return true if !target_spot_piece.nil? && target_spot_piece.color == color
+  end
+
+  def diagonal_move?(start_spot, end_spot)
+    return true if ((start_spot[0] - end_spot[0]).abs - (start_spot[1] - end_spot[1]).abs).zero?
+
+    false
+  end
 end
