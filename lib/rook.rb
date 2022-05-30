1012e1bcadc2660fb04f4574479072f481831e9e
Brittany Woedl <bwoedl4@gmail.com>
2022-05-30 11:27:19 -0500

    Update legal_move? to check if intermediate spaces are occupied

--- lib/rook.rb
+++ lib/rook.rb
@@ -1,16 +1,51 @@
 class Rook < Piece
-  attr_accessor :symbol
+  attr_accessor :symbol, :move
 
   def initialize(color)
     super(color)
     @symbol = set_symbol
+    @move = 1
   end
 
   def set_symbol
     color == 'white' ? '♜'.bold : '♜'.black
   end
 
-  def self.legal_move?(start_spot, end_spot)
-    start_spot[0] == end_spot[0] || start_spot[1] == end_spot[1]
+  def legal_move?(board, start_spot, end_spot)
+    return false if occupied_by_same_color?(board, end_spot)
+
+    row_move = start_spot[0] - end_spot[0]
+    col_move = start_spot[1] - end_spot[1]
+
+    if col_move.zero?
+      return true if check_intermediate_row_spots(board, start_spot[0], end_spot[0])
+    elsif row_move.zero?
+      return true if check_intermediate_col_spots(board, start_spot[1], end_spot[1])
+    else
+      false
+    end
+  end
+
+  # see if following two methods can be combined / abstracted 
+  def check_intermediate_row_spots(board, start_spot, end_spot)
+    (min(start_spot, end_spot)...max(start_spot, end_spot)).each do |spot|
+      return false unless board.get_piece([spot, start_spot]).nil?
+    end
+    true
+  end
+
+  def check_intermediate_col_spots(board, start_spot, end_spot)
+    (min(start_spot, end_spot)...max(start_spot, end_spot)).each do |spot|
+      return false unless board.get_piece([start_spot[0], spot]).nil?
+    end
+    true
+  end
+
+  def min(start_spot, end_spot)
+    [start_spot, end_spot].min
+  end
+
+  def max(start_spot, end_spot)
+    [start_spot, end_spot].max
   end
 end
--- spec/rook_spec.rb
+++ spec/rook_spec.rb
@@ -4,20 +4,26 @@ require 'piece'
 require 'rook'
 
 describe Rook do
+  subject(:Rook) { Rook.new('white') }
+  let(:board) { double('board') }
+
   describe '.legal_move' do
     context 'moves diagonally' do
       it 'returns false' do
-        expect(Rook.legal_move?([4, 3], [5, 4])).to be(false)
+        allow(board).to receive(:get_piece).and_return(nil)
+        expect(subject.legal_move?(board, [4, 3], [5, 4])).to be(false)
       end
     end
     context 'moves one space to the side' do
       it 'returns true' do
-        expect(Rook.legal_move?([4, 3], [4, 4])).to be(true)
+        allow(board).to receive(:get_piece).and_return(nil)
+        expect(subject.legal_move?(board, [4, 3], [4, 4])).to be(true)
       end
     end
     context 'moves multiple spaces to the side' do
       it 'returns true' do
-        expect(Rook.legal_move?([1, 3], [7, 3])).to be(true)
+        allow(board).to receive(:get_piece).and_return(nil)
+        expect(subject.legal_move?(board, [1, 3], [7, 3])).to be(true)
       end
     end
   end
