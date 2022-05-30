6af44615829982a27636dbad9b7e70112d541ff2
Brittany Woedl <bwoedl4@gmail.com>
2022-05-30 11:28:15 -0500

    Update queen legal_move to check if target spot occupied by the same color

--- lib/queen.rb
+++ lib/queen.rb
@@ -1,16 +1,18 @@
 class Queen < Piece
-  attr_accessor :symbol
+  attr_accessor :symbol, :move
 
   def initialize(color)
     super(color)
     @symbol = set_symbol
+    @move = 1
   end
 
   def set_symbol
     color == 'white' ? '♚'.bold : '♚'.black
   end
 
-  def self.legal_move?(start_spot, end_spot)
+  def legal_move?(board, start_spot, end_spot)
+    return false if occupied_by_same_color?(board, end_spot)
     return true if start_spot[0] == end_spot[0] || start_spot[1] == end_spot[1]
     return true if ((start_spot[0] - end_spot[0]).abs - (start_spot[1] - end_spot[1]).abs).zero?
 
--- spec/queen_spec.rb
+++ spec/queen_spec.rb
@@ -4,20 +4,26 @@ require 'piece'
 require 'queen'
 
 describe Queen do
+  subject(:queen) { Queen.new('white') }
+  let(:board) { double('board') }
+
   describe '.legal_move' do
     context 'moves two spaces up and one to the side' do
       it 'returns false' do
-        expect(Queen.legal_move?([3, 3], [5, 4])).to be(false)
+        allow(board).to receive(:get_piece).and_return(nil)
+        expect(subject.legal_move?(board, [3, 3], [5, 4])).to be(false)
       end
     end
     context 'moves to the side' do
       it 'returns true' do
-        expect(Queen.legal_move?([4, 3], [4, 7])).to be(true)
+        allow(board).to receive(:get_piece).and_return(nil)
+        expect(subject.legal_move?(board, [4, 3], [4, 7])).to be(true)
       end
     end
     context 'moves diagonally' do
       it 'returns true' do
-        expect(Queen.legal_move?([4, 3], [7, 6])).to be(true)
+        allow(board).to receive(:get_piece).and_return(nil)
+        expect(subject.legal_move?(board, [4, 3], [7, 6])).to be(true)
       end
     end
   end
