8161014f3c9be343a151e9ef7dc5ef159163bab0
BWoedl <36779121+BWoedl@users.noreply.github.com>
2022-05-31 17:54:13 -0500

    Add additional tests

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,11 +3,16 @@
 require 'game'
 
 describe Game do
-  let(:board) { double('board') }
-  let(:spot) { double('spot') }
-  let(:king) { double('king', class: King, color: 'white') }
-  let(:player1) { double('player1', name: 'Dingo', color: 'white') }
-  let(:player2) { double('player2', name: 'Huckleberry', color: 'black') }
+  let(:board) { Board.new }
+  let(:spot) { double('spot', piece: ) }
+  let(:king) { double('king', class: King, color: 'white', move: 1, defeated: false) }
+  let(:queen) { double('queen', class: Queen, color: 'black', move: 1, defeated: false) }
+  let(:pawn) { double('pawn', class: Pawn, color: 'white', move: 1, defeated: false) }
+  let(:spot) { double('spot', piece: Pawn) }
+  let(:spotk) { double('spot', piece: king) }
+  let(:spotq) { double('spot', piece: queen) }
+  let(:player1) { double('player', name: 'Dingo', color: 'white') }
+  let(:player2) { double('player', name: 'Huckleberry', color: 'black') }
   subject(:game) { described_class.new(player1, player2, board) }
 
   context 'initializer' do
@@ -115,15 +120,78 @@ describe Game do
   end
 
   describe '.take_turn' do
-    context 'something' do
-      xit 'something do' do
+    context 'when turn is not valid' do
+      before do
+        allow(subject).to receive(:gets).and_return('c8', 'd8')
+        allow(board).to receive(:grid).and_return([[spot, spot, spot, spot, spot, spot, spot],
+                                                   [spot, spot, spot, spot, spot, spot, spot],
+                                                   [spot, spot, spot, spot, spot, spot, spot],
+                                                   [spot, spot, spot, spot, spot, spot, spot],
+                                                   [spot, spot, spot, spot, spot, spot, spot],
+                                                   [spot, spot, spot, spot, spot, spot, spot],
+                                                   [spot, spot, spot, spot, spot, spot, spot],
+                                                   [spot, spot, spotk, spotq, spot, spot, spot]])
+        allow(board).to receive(:get_piece).and_return(king)
+        allow(king).to receive(:legal_move?).and_return(false, true)
+        # allow(king).to receive(:move=)
+        # allow(subject).to receive(:defeat_piece).with(queen, nil)
+        # allow(queen).to receive(:defeated=)
+        # allow(board).to receive(:display)
+      end
+      it 'puts illegal move message' do
+        subject.instance_variable_set(:@turn, 1)
+        # allow(subject).to receive(:valid_turn?).and_return(false, true)
+        expect(subject.take_turn).to receive(:puts)
+        subject.take_turn
+      end
+      xit 'restarts the loop' do
+        expect(subject.take_turn).to receive(:update_board).once
+        subject.take_turn
+      end
+    end
+    context 'when turn is valid' do
+      # allow(subject).to receive(:valid_turn?).and_return(false)
+      xit 'calls to update the board' do
+      end
+      xit 'stops the loop' do
       end
     end
   end
 
   describe '.update_board' do
-    context 'something' do
-      xit 'something' do
+    context 'when piece is not a pawn' do
+      before do
+        allow(subject).to receive(:move_piece)
+        allow(board).to receive(:display)
+        allow(king).to receive(:move=)
+      end
+      it 'increments the turn counter' do
+        subject.instance_variable_set(:@turn, 1)
+        subject.update_board(king, [0, 0], [0, 1])
+        expect(subject.instance_variable_get(:@turn)).to eq(2)
+      end
+      it 'passant_spot remains as nil' do
+        expect(subject).to receive(:move_piece).with(king, [0, 0], [0, 1], nil)
+        subject.update_board(king, [0, 0], [0, 1])
+      end
+    end
+    context 'when piece is a pawn' do 
+      before do
+        allow(subject).to receive(:move_piece)
+        allow(board).to receive(:display)
+        allow(pawn).to receive(:move=)
+        allow(pawn).to receive(:instance_of?).and_return(true)
+      end
+      it 'assigns passant_spot if it is a valid passing move' do
+        allow(pawn).to receive(:en_passant?).and_return(true) 
+        allow(pawn).to receive(:en_passant_spot).and_return([4, 5]).once
+        expect(subject).to receive(:move_piece).with(pawn, [4, 4], [5, 5], [4, 5])
+        subject.update_board(pawn, [4, 4], [5, 5])
+      end
+      it 'does not assign passant_spot if it is an invalid passing move' do 
+        allow(pawn).to receive(:en_passant?).and_return(false)
+        expect(subject).to receive(:move_piece).with(pawn, [4, 4], [5, 5], nil)
+        subject.update_board(pawn, [4, 4], [5, 5])
       end
     end
   end
