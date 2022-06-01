c4e209e7f39b6815385ff763f37b7923310026bc
Brittany Woedl <bwoedl4@gmail.com>
2022-05-31 22:14:40 -0500

    Correct the grid size in the board tests

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -4,7 +4,7 @@ require 'board'
 require 'spot'
 
 describe Board do
-  subject(:board) { Board.new }  
+  subject(:board) { Board.new }
   let(:king) { double('king', class: King) }
   let(:rook) { double('rook', class: Rook) }
   let(:spot) { double('spot') }
@@ -48,24 +48,28 @@ describe Board do
   describe '.get_piece' do
     context 'when a king is in the spot on the board' do
       it 'returns king class' do
-        allow(subject).to receive(:grid).and_return([[spot2, spot, spot, spot, spot1, spot, spot],
-                                                     [spot, spot, spot, spot, spot, spot, spot],
-                                                     [spot, spot, spot, spot, spot, spot, spot],
-                                                     [spot, spot, spot, spot, spot, spot, spot],
-                                                     [spot, spot, spot, spot, spot, spot, spot],
-                                                     [spot, spot, spot, spot, spot, spot, spot]])
+        allow(subject).to receive(:grid).and_return([[spot2, spot, spot, spot, spot1, spot, spot, spot],
+                                                     [spot, spot, spot, spot, spot, spot, spot, spot],
+                                                     [spot, spot, spot, spot, spot, spot, spot, spot],
+                                                     [spot, spot, spot, spot, spot, spot, spot, spot],
+                                                     [spot, spot, spot, spot, spot, spot, spot, spot],
+                                                     [spot, spot, spot, spot, spot, spot, spot, spot],
+                                                     [spot, spot, spot, spot, spot, spot, spot, spot],
+                                                     [spot, spot, spot, spot, spot, spot, spot, spot]])
         expect(subject.get_piece([0, 4])).to match(King)
       end
     end
 
     context 'when a rook is in the spot on the board ' do
       it 'returns rook class' do
-        allow(subject).to receive(:grid).and_return([[spot2, spot, spot, spot, spot1, spot, spot],
-                                                     [spot, spot, spot, spot, spot, spot, spot],
-                                                     [spot, spot, spot, spot, spot, spot, spot],
-                                                     [spot, spot, spot, spot, spot, spot, spot],
-                                                     [spot, spot, spot, spot, spot, spot, spot],
-                                                     [spot, spot, spot, spot, spot, spot, spot]])
+        allow(subject).to receive(:grid).and_return([[spot2, spot, spot, spot, spot1, spot, spot, spot],
+                                                     [spot, spot, spot, spot, spot, spot, spot, spot],
+                                                     [spot, spot, spot, spot, spot, spot, spot, spot],
+                                                     [spot, spot, spot, spot, spot, spot, spot, spot],
+                                                     [spot, spot, spot, spot, spot, spot, spot, spot],
+                                                     [spot, spot, spot, spot, spot, spot, spot, spot],
+                                                     [spot, spot, spot, spot, spot, spot, spot, spot],
+                                                     [spot, spot, spot, spot, spot, spot, spot, spot]])
         expect(subject.get_piece([0, 0])).to match(Rook)
       end
     end
