b93f9fda5ae17bb1bdd2538ce526957d9daf868d
Brittany Woedl <bwoedl4@gmail.com>
2022-06-04 09:14:13 -0500

    Add check? and helper methods

--- lib/board.rb
+++ lib/board.rb
@@ -23,9 +23,8 @@ class Board
   end
 
   def get_piece(spot)
-    unless @grid[spot[0]][spot[1]].nil? 
-      return @grid[spot[0]][spot[1]].piece
-    end
+    return @grid[spot[0]][spot[1]].piece unless @grid[spot[0]][spot[1]].nil?
+
     nil
   end
 
@@ -55,24 +54,52 @@ class Board
 
   def display
     puts `clear`
+    display_block_lines
+    puts "\nThe black king is in check!" if check?('black') == true
+    puts "\nThe white king is in check" if check?('white') == true
+  end
+
+  # return to this to reduce complexity
+  def display_block_lines
     line = "   a      b      c      d      e      f      g      h\n\n".bold
     @grid.reverse.each do |row|
-      line += display_block_lines(row)
+      3.times do |index|
+        row.each do |spot|
+          line += (spot.x.even? && spot.y.even?) || (spot.x.odd? && spot.y.odd?) ? CYAN_BG : GRAY_BG
+          index == 1 && !spot.piece.nil? ? line[-8] = spot.piece.symbol : line
+          index == 1 && spot.y == 7 ? line += "    #{spot.x + 1}".bold : line
+        end
+        line += "\n"
+      end
     end
     puts line
   end
 
-  # return to this to reduce complexity
-
-  def display_block_lines(row, line = '')
-    3.times do |index|
+  def active_opponent_spots(color)
+    spots = []
+    @grid.each do |row|
       row.each do |spot|
-        line += (spot.x.even? && spot.y.even?) || (spot.x.odd? && spot.y.odd?) ? CYAN_BG : GRAY_BG
-        index == 1 && !spot.piece.nil? ? line[-8] = spot.piece.symbol : line
-        index == 1 && spot.y == 7 ? line += "    #{spot.x + 1}".bold : line
+        spots << spot unless spot.piece.nil? || spot.piece.color == color
       end
-      line += "\n"
     end
-    line
+    spots
+  end
+
+  def check?(color)
+    king_spot = find_king_spot(color)
+    spots = active_opponent_spots(color)
+    spots.each do |spot|
+      return true if spot.piece.legal_move?(self, [spot.x, spot.y], [king_spot.x, king_spot.y])
+    end
+    false
+  end
+
+  def find_king_spot(color)
+    @grid.each do |row|
+      row.each do |spot|
+        return spot if !spot.piece.nil? && spot.piece.instance_of?(King) && spot.piece.color == color
+      end
+    end
+    nil
   end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -5,11 +5,23 @@ require 'spot'
 
 describe Board do
   subject(:board) { Board.new }
-  let(:king) { double('king', class: King) }
-  let(:rook) { double('rook', class: Rook) }
-  let(:spot) { double('spot') }
-  let(:spot1) { double('spot', piece: king) }
-  let(:spot2) { double('spot', piece: rook) }
+  let(:white_king) { double('king', class: King, color: 'white') }
+  let(:white_rook) { double('rook', class: Rook, color: 'white') }
+  let(:white_queen) { double('queen', class: Queen, color: 'white') }
+  let(:white_knight) { double('knight', class: Knight, color: 'white') }
+  let(:white_bishop) { double('bishop', class: Bishop, color: 'white') }
+  let(:white_pawn) { double('pawn', class: Pawn, color: 'white') }
+  let(:black_rook) { double('rook', class: Rook, color: 'black') }
+  let(:black_king) { double('king', class: King, color: 'black') }
+  let(:spot) { double('spot', piece: nil) }
+  let(:k_spot_b) { double('spot', piece: black_king) }
+  let(:r_spot_b) { double('spot', piece: black_rook) }
+  let(:k_spot_w) { double('spot', piece: white_king) }
+  let(:r_spot_w) { double('spot', piece: white_rook) }
+  let(:q_spot_w) { double('spot', piece: white_queen) }
+  let(:n_spot_w) { double('spot', piece: white_knight) }
+  let(:b_spot_w) { double('spot', piece: white_bishop) }
+  let(:p_spot_w) { double('spot', piece: white_pawn) }
 
   describe '.create_board' do
     context 'resets the board to an empty 8x8 grid' do
@@ -34,11 +46,11 @@ describe Board do
         grid = subject.instance_variable_get(:@grid)
         expect(grid[1][6].piece).to be_a(Pawn)
       end
-      it 'adds king to the fifth column on each side' do 
+      it 'adds king to the fifth column on each side' do
         grid = subject.instance_variable_get(:@grid)
         expect(grid[0][4].piece).to be_a(King)
       end
-      it 'adds queen to the fourth column on each side' do 
+      it 'adds queen to the fourth column on each side' do
         grid = subject.instance_variable_get(:@grid)
         expect(grid[7][3].piece).to be_a(Queen)
       end
@@ -48,29 +60,133 @@ describe Board do
   describe '.get_piece' do
     context 'when a king is in the spot on the board' do
       it 'returns king class' do
-        allow(subject).to receive(:grid).and_return([[spot2, spot, spot, spot, spot1, spot, spot, spot],
-                                                     [spot, spot, spot, spot, spot, spot, spot, spot],
-                                                     [spot, spot, spot, spot, spot, spot, spot, spot],
-                                                     [spot, spot, spot, spot, spot, spot, spot, spot],
-                                                     [spot, spot, spot, spot, spot, spot, spot, spot],
-                                                     [spot, spot, spot, spot, spot, spot, spot, spot],
-                                                     [spot, spot, spot, spot, spot, spot, spot, spot],
-                                                     [spot, spot, spot, spot, spot, spot, spot, spot]])
-        expect(subject.get_piece([0, 4])).to match(King)
+        subject.instance_variable_set(:@grid, [[r_spot_b, spot, spot, spot, k_spot_b, spot, spot, spot],
+                                               [spot, spot, spot, spot, spot, spot, spot, spot],
+                                               [spot, spot, spot, spot, spot, spot, spot, spot],
+                                               [spot, spot, spot, spot, spot, spot, spot, spot],
+                                               [spot, spot, spot, spot, spot, spot, spot, spot],
+                                               [spot, spot, spot, spot, spot, spot, spot, spot],
+                                               [spot, spot, spot, spot, spot, spot, spot, spot],
+                                               [spot, spot, spot, spot, spot, spot, spot, spot]])
+        expect(subject.get_piece([0, 4])).to match(black_king)
       end
     end
 
     context 'when a rook is in the spot on the board ' do
       it 'returns rook class' do
-        allow(subject).to receive(:grid).and_return([[spot2, spot, spot, spot, spot1, spot, spot, spot],
-                                                     [spot, spot, spot, spot, spot, spot, spot, spot],
-                                                     [spot, spot, spot, spot, spot, spot, spot, spot],
-                                                     [spot, spot, spot, spot, spot, spot, spot, spot],
-                                                     [spot, spot, spot, spot, spot, spot, spot, spot],
-                                                     [spot, spot, spot, spot, spot, spot, spot, spot],
-                                                     [spot, spot, spot, spot, spot, spot, spot, spot],
-                                                     [spot, spot, spot, spot, spot, spot, spot, spot]])
-        expect(subject.get_piece([0, 0])).to match(Rook)
+        subject.instance_variable_set(:@grid, [[r_spot_b, spot, spot, spot, k_spot_b, spot, spot, spot],
+                                               [spot, spot, spot, spot, spot, spot, spot, spot],
+                                               [spot, spot, spot, spot, spot, spot, spot, spot],
+                                               [spot, spot, spot, spot, spot, spot, spot, spot],
+                                               [spot, spot, spot, spot, spot, spot, spot, spot],
+                                               [spot, spot, spot, spot, spot, spot, spot, spot],
+                                               [spot, spot, spot, spot, spot, spot, spot, spot],
+                                               [spot, spot, spot, spot, spot, spot, spot, spot]])
+        expect(subject.get_piece([0, 0])).to match(black_rook)
+      end
+    end
+  end
+
+  describe 'active_piece_spots' do
+    context 'when the board has just been setup and white is specified as an argument' do
+      it 'returns all 16 spots with white pieces' do
+        subject.instance_variable_set(:@grid, [[spot, spot, spot, spot, spot, spot, spot, spot],
+                                               [spot, spot, spot, spot, spot, spot, spot, spot],
+                                               [spot, spot, spot, spot, spot, spot, spot, spot],
+                                               [spot, spot, spot, spot, spot, spot, spot, spot],
+                                               [spot, spot, spot, spot, spot, spot, spot, spot],
+                                               [spot, spot, spot, spot, spot, spot, spot, spot],
+                                               [p_spot_w, p_spot_w, p_spot_w, p_spot_w, p_spot_w, p_spot_w, p_spot_w, p_spot_w],
+                                               [r_spot_w, n_spot_w, b_spot_w, q_spot_w, k_spot_w, b_spot_w, n_spot_w, r_spot_w]])
+        expect(subject.active_opponent_spots('black').count).to eq(16)
+      end
+    end
+    context 'when the white rooks have been defeated' do
+      it 'does not return any spots with those pieces' do
+        subject.instance_variable_set(:@grid, [[spot, spot, spot, spot, spot, spot, spot, spot],
+                                               [spot, spot, spot, spot, spot, spot, spot, spot],
+                                               [spot, spot, spot, spot, spot, spot, spot, spot],
+                                               [spot, spot, spot, spot, spot, spot, spot, spot],
+                                               [spot, spot, spot, spot, spot, spot, spot, spot],
+                                               [spot, spot, spot, spot, spot, spot, spot, spot],
+                                               [p_spot_w, p_spot_w, p_spot_w, p_spot_w, p_spot_w, p_spot_w, p_spot_w, p_spot_w],
+                                               [spot, n_spot_w, b_spot_w, q_spot_w, k_spot_w, b_spot_w, n_spot_w, spot]])
+        expect(subject.active_opponent_spots('black')).not_to include(r_spot_w)
+      end
+    end
+    context 'when black is specified as an argument' do
+      it 'does not return any spots with white pieces' do
+        subject.instance_variable_set(:@grid, [[spot, spot, spot, spot, spot, spot, spot, spot],
+                                               [spot, spot, spot, spot, spot, spot, spot, spot],
+                                               [spot, spot, spot, spot, spot, spot, spot, spot],
+                                               [spot, spot, spot, spot, spot, spot, spot, spot],
+                                               [spot, spot, spot, spot, spot, spot, spot, spot],
+                                               [spot, spot, spot, spot, spot, spot, spot, spot],
+                                               [p_spot_w, p_spot_w, p_spot_w, p_spot_w, p_spot_w, p_spot_w, p_spot_w, p_spot_w],
+                                               [r_spot_w, n_spot_w, b_spot_w, q_spot_w, k_spot_w, b_spot_w, n_spot_w, r_spot_w]])
+        expect(subject.active_opponent_spots('white').count).to eq(0)
+      end
+    end
+  end
+
+  describe 'find_opponent_king_spot' do
+    context 'it returns the spot with the king in it' do
+      it 'returns the spot that has the king piece with the specified color' do
+        subject.instance_variable_set(:@grid, [[spot, spot, spot, spot, spot, spot, spot, spot],
+                                               [spot, spot, spot, spot, spot, spot, spot, spot],
+                                               [spot, spot, spot, spot, spot, spot, spot, spot],
+                                               [spot, spot, spot, spot, spot, spot, spot, spot],
+                                               [spot, spot, spot, spot, spot, spot, spot, spot],
+                                               [spot, spot, spot, spot, spot, spot, spot, spot],
+                                               [p_spot_w, p_spot_w, p_spot_w, p_spot_w, p_spot_w, p_spot_w, p_spot_w, p_spot_w],
+                                               [r_spot_w, n_spot_w, b_spot_w, q_spot_w, k_spot_w, b_spot_w, n_spot_w, r_spot_w]])
+        allow(k_spot_w.piece).to receive(:instance_of?).with(King).and_return true
+        expect(subject.find_king_spot('white')).to eq(k_spot_w)
+      end
+    end
+  end
+
+  describe '.check?' do
+    context 'when a rook has a legal move which would capture the king' do
+      before do
+        subject.instance_variable_set(:@grid, [[spot, spot, spot, spot, spot, spot, spot, spot],
+                                               [spot, spot, spot, spot, spot, spot, spot, spot],
+                                               [spot, spot, spot, spot, spot, spot, spot, spot],
+                                               [spot, spot, spot, spot, spot, spot, spot, spot],
+                                               [spot, spot, spot, spot, spot, spot, spot, spot],
+                                               [spot, spot, spot, spot, r_spot_b, spot, spot, spot],
+                                               [p_spot_w, p_spot_w, p_spot_w, p_spot_w, spot, p_spot_w, p_spot_w, p_spot_w],
+                                               [r_spot_w, n_spot_w, b_spot_w, q_spot_w, k_spot_w, b_spot_w, n_spot_w, r_spot_w]])
+        allow(k_spot_w.piece).to receive(:instance_of?).with(King).and_return true
+        allow(r_spot_b).to receive(:x).and_return(5)
+        allow(r_spot_b).to receive(:y).and_return(4)
+        allow(k_spot_w).to receive(:x).and_return(7)
+        allow(k_spot_w).to receive(:y).and_return(4)
+        allow(black_rook).to receive(:legal_move?).and_return true
+      end
+      it 'returns true' do
+        expect(subject.check?('white')).to be true
+      end
+    end
+    context 'when there are no legal moves which could capture the king' do
+      before do
+        subject.instance_variable_set(:@grid, [[spot, spot, spot, spot, spot, spot, spot, spot],
+                                               [spot, spot, spot, spot, spot, spot, spot, spot],
+                                               [spot, spot, spot, spot, spot, spot, spot, spot],
+                                               [spot, spot, spot, spot, spot, spot, spot, spot],
+                                               [spot, spot, spot, spot, r_spot_b, spot, spot, spot],
+                                               [spot, spot, spot, spot, p_spot_w, spot, spot, spot],
+                                               [p_spot_w, p_spot_w, spot, p_spot_w, spot, p_spot_w, p_spot_w, p_spot_w],
+                                               [r_spot_w, n_spot_w, b_spot_w, q_spot_w, k_spot_w, b_spot_w, n_spot_w, r_spot_w]])
+        allow(k_spot_w.piece).to receive(:instance_of?).with(King).and_return true
+        allow(r_spot_b).to receive(:x).and_return(4)
+        allow(r_spot_b).to receive(:y).and_return(4)
+        allow(k_spot_w).to receive(:x).and_return(7)
+        allow(k_spot_w).to receive(:y).and_return(4)
+        allow(black_rook).to receive(:legal_move?).and_return false
+      end
+      it 'returns false' do
+        expect(subject.check?('white')).to be false
       end
     end
   end
